d889a69e43606f4e1e6fe33f1a3af230c2def4de
allanhicks <fishmanHicks@gmail.com>
2015-06-28 12:48:24 -0700

    Added sex ratios to SS3LFstrata.fn

--- R/SS3LFstrata.fn.R
+++ R/SS3LFstrata.fn.R
@@ -1,5 +1,5 @@
 SS3LFstrata.fn <-
-function(len,Strata="Year",lgthBins=1,gender=3,nSamps="EnterNsamps",fleet="EnterFleet",season=1,partition=0,NAs2zero=T) {
+function(len,Strata="Year",lgthBins=1,gender=3,nSamps="EnterNsamps",fleet="EnterFleet",season=1,partition=0,NAs2zero=T,sexRatioUnsexed=NA,maxSizeUnsexed=NA) {
     #calculates proportion at length and reformats into SS# format
     #Gender=0: sexes combined and entered in female placeholders. Male values ignored
     #Gender=1: females only. Male values ignored
@@ -13,7 +13,50 @@ function(len,Strata="Year",lgthBins=1,gender=3,nSamps="EnterNsamps",fleet="Enter
     #The NAs2zero determines if NA values will be changed to 0.0.
     #     You will want this to be T when inputting into SS3, but may want it to be false when plotting
     #     Note that an NA means there were no males and females recorded. Therefore you may get a zero if only one sex was recorded
-    # NOTICE: UNSEXED CORRECTION NOT IMPLEMENTED
+    #Sex Ratio for unsexed
+    #     sexRatioUnsexed is the proportion used to assign unsexed fish to females
+    #       -- if a single number, it is only used for sizes at or below maxSizeUnsexed, and sex ratio for size bins above that are calculated using the 
+    #           number of males and females observed in the same size class (or one lower if none available in the same bin)
+    #       -- if a vector (maybe to be implemented), it must be the same length as the lgthBins and indicates the proportion of unsexed assigned to females for each length bin
+    #       -- if it is NA, unsexed fish are omitted WHEN GENDER=3 (THIS IS THE DEFAULT)
+    #     maxSizeUnsexed determines the maximum size at which the sexRatioUnsexed is applied. If sexRatioUnsexed is a vector, this is ignored
+
+
+    if(length(sexRatioUnsexed)==1 & !is.na(sexRatioUnsexed)) {
+        len$sexRatio <- len$NumF/(len$NumF+len$NumM)
+        len$sexRatio[len$Length <= maxSizeUnsexed] <- sexRatioUnsexed
+        #now fill in any missing ratios with ratios of that bin from other years and strata (can probably be done more efficiently)
+        noRatio <- which(is.na(len$sexRatio))
+        if(length(noRatio)>0) cat("\nThese are sex ratios that were filled in using observations from the same lengths from different strata and years\n")
+        for(i in noRatio) {
+            inds <- len$allLs==len$allLs[i]
+            tmpF <- sum(len$NumF[inds])
+            tmpM <- sum(len$NumM[inds])
+            len$sexRatio[i] <- tmpF/(tmpF+tmpM)
+            print(len[i,c("Length","allLs","NumF","NumM","sexRatio")])
+        }
+
+        noRatio <- which(is.na(len$sexRatio))
+        if(length(noRatio)>0) cat("\nThese are sex ratios that were filled in using observations from nearby lengths\n")
+        for(i in noRatio) {
+            nearLens <- Lengths[c(which(Lengths==len$allLs[i])-1,which(Lengths==len$allLs[i])+1)]
+            inds <- len$allLs %in% nearLens
+            tmpF <- sum(len$NumF[inds])
+            tmpM <- sum(len$NumM[inds])
+            len$sexRatio[i] <- tmpF/(tmpF+tmpM)
+            print(len[i,c("Length","allLs","NumF","NumM","sexRatio")])
+        }
+        noRatio <- which(is.na(len$sexRatio))
+        if(length(noRatio)>0) cat("Some sex ratios were left unknown and omitted\n\n")
+        if(length(noRatio)==0) cat("Done filling in sex ratios\n\n")
+
+        tmpFemUnsex <- round(len$sexRatio*len$NumUnsexed)
+        tmpMaleUnsex <- len$NumUnsexed - tmpFemUnsex
+        len$NumF <- len$NumF + tmpFemUnsex
+        len$NumM <- len$NumM + tmpMaleUnsex
+        #print(unique(round(len$sexRatio,1)))
+    }
+
     xx <- split(len[,c("Length","NumF","NumM","NumUnsexed")],len[,Strata])
     years <- names(xx)
     if(length(lgthBins)==1) {
