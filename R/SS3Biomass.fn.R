7ce5faa5366dfdf772bb3d4b679580b19f90ab38
Chantel Wetzel <chantel.wetzel@noaa.gov>
2018-04-17 14:56:23 -0700

    Updated the excel biomass functions
    Switched excel reading packages, removed column name renaming, and modified to use the original column names.

--- R/SS3Biomass.fn.R
+++ R/SS3Biomass.fn.R
@@ -7,24 +7,29 @@
 #' @param season season
 #' @param outputMedian TRUE/FALSE
 #'
-#' @author Allan Hicks 
+#' @author Allan Hicks & Chantel Wetzel
 #' @export
 
 SS3Biomass.fn <-function(bio, fleet="EnterFleet", season=1, outputMedian=T) {
 
-    xxx <- split(bio[,c("AreaName","MinStratumDepth","MaxStratumDepth","Biomass","BiomassVar")],bio$Year)
+    xxx <- split(bio[,c("Area_Name","Min_Stratum_Depth_(m)","Max_Stratum_Depth_(m)","Biomass_(kg)","Biomass_Variance")],bio[,"Survey_Year"])
     years <- names(xxx)
-    bio <- unlist(lapply(xxx,function(x){sum(x$Biomass,na.rm=T)}))
-    variance <- unlist(lapply(xxx,function(x){sum(x$BiomassVar,na.rm=T)}))
+
+    #bio <- unlist(lapply(xxx,function(x){sum(x$Biomass, na.rm=T)}))
+    #variance <- unlist(lapply(xxx,function(x){sum(x$BiomassVar,na.rm=T)}))
+    bio <- unlist(lapply(xxx,function(x){sum(x[,"Biomass_(kg)"], na.rm=T)}))
+    variance <- unlist(lapply(xxx,function(x){sum(x[,"Biomass_Variance"],na.rm=T)}))
+
     cv <- sqrt(variance)/bio
     seLogB <- sqrt(log(cv^2+1))
     if(outputMedian==T) {
         med <- bio*exp(-0.5*seLogB^2)
-        out <- data.frame(Year=years,Season=season,Fleet=fleet,Value=med,seLogB=seLogB)
+        out <- data.frame(Year=years, Season=season, Fleet=fleet, Value=med, seLogB=seLogB)
     }
     else {
         out <- data.frame(Year=years,Season=season,Fleet=fleet,Value=bio,seLogB=seLogB)
     }
     row.names(out) <- NULL
     return(out)
+
 }
--- R/readInExcelBiomass.fn.R
+++ R/readInExcelBiomass.fn.R
@@ -1,28 +1,30 @@
 #' Reads in the stratum biomasses from the Excel file provided by Beth (BiomassAbundance Sheet)
 #' headerRow is the row number of the column where the data start
-#' it doesn't read in the column names correctly, so I put in simplified names.
-#' Make sure that these match what is in your Excel spreadsheet
+#' 
 #' 
 #' @param file excel file name
 #' @param sheet sheet name in excel file
 #' @param headerRow line of header row in excel file
-#' @param colNames column names, the function replaces the column names
 #'
-#' @author Allan Hicks 
+#' @author Allan Hicks & Chantel Wetzel
 #' @export 
 #' \code{\link{readDataFromExcel.fn}}
 
-readInExcelBiomass.fn <-function(file,sheet="BiomassAbundance",headerRow=6,colNames=NA) {
+
+readInExcelBiomass.fn <-function(file, sheet="BiomassAbundance", headerRow=6) { #, colNames=NA) {
 
     xx <- readDataFromExcel.fn(file,sheet,headerRow)
-    if(is.null(colNames[1])) {
-        nombres <- c("Species","ScientificName","SpeciesCode","Year","Project","StrataAreaVersion","AreaSetId","AreaName","SouthernLatitude","NorthernLatitude","DepthStrataSet","MinStratumDepth","MaxStratumDepth","StratumArea","Biomass","Abundance","CpueWeightVar","CpueCountVar","BiomassVar","CV","N","Nbio","Npos","NbioPos")
-        names(xx) <- nombres
-        cat("NOTE: column names have been modified from the Excel Sheet. You may want to verify that they match.\n")
-    }
-    if(!is.na(colNames[1])) {
-        names(xx) <- colNames
-        return(xx)
-    }
+    #if(is.null(colNames[1])) {
+    #    nombres <- c("Species","ScientificName","SpeciesCode","Year","Project","StrataAreaVersion",
+    #                 "AreaSetId","AreaName","SouthernLatitude","NorthernLatitude","DepthStrataSet",
+    #                 "MinStratumDepth","MaxStratumDepth","StratumArea","Biomass","Abundance",
+    #                 "CpueWeightVar","CpueCountVar","BiomassVar","CV","N","Nbio","Npos","NbioPos")
+    #    names(xx) <- nombres
+    #    cat("NOTE: column names have been modified from the Excel Sheet. You may want to verify that they match.\n")
+    #}
+    #if(!is.na(colNames[1])) {
+    #    names(xx) <- colNames
+    #    return(xx)
+    #}
     return(xx)
 }
