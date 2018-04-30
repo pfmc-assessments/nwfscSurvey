517860ed0b3b92e2a681fd760c5c9e7a2f2ce91b
Chantel Wetzel <chantel.wetzel@noaa.gov>
2018-04-30 10:08:56 -0700

    In development version of getN function
    Currently does not work due to column names

--- R/getN.R
+++ R/getN.R
@@ -2,6 +2,7 @@
 #' based on Stewart & Hamel 2014
 #' 
 #' @param file excel file name
+#' @param type specify whether doing "length" or "age". Used to read associatied excel sheets
 #' @param sheet sheet name in excel file
 #' @param headerRow line of header row in excel file
 #' @param species species specific value to determine the number of unique samples per tow (flatfish, shelfrock, sloperock, thorny, others, all)
@@ -10,7 +11,7 @@
 #' @export 
 #' \code{\link{readDataFromExcel.fn}}
 
-getN <- function(dir, file, headerRow, species = "flatfish", printfolder = "forSS"){
+getN <- function(dir, file, type, headerRow = "default", species = "flatfish", printfolder = "forSS"){
 
 	if (species == "flatfish")  {n.unq = 3.09 }
 	if (species == "shelfrock") {n.unq = 2.43 }
@@ -19,13 +20,23 @@ getN <- function(dir, file, headerRow, species = "flatfish", printfolder = "forS
 	if (species == "others")    {n.unq = 2.38 }
 	if (species == "all")       {n.unq = 2.73 }
 
-	hauls = readInExcelBiomass.fn(file, sheet = "Lengths-StratumTowTallies", header = 7)
-	len.num = readInExcelBiomass.fn(file, sheet = "Lengths", header = 9)
+	if (type = "length"){
+		if (headerRow = "default") { headerRow = c(7,9) }
+		hauls = readDataFromExcel.fn(file, sheet = "Lengths-StratumTowTallies", header = headerRow[1])
+		len.num = readDataFromExcel.fn(file, sheet = "Lengths", header = headerRow[2])		
+	}
+
+	if (type = "age"){
+		if (headerRow = "default") { headerRow = c(7,9) }
+		hauls = readDataFromExcel.fn(file, sheet = "Ages-StratumTowTallies", header = 7)
+		len.num = readDataFromExcel.fn(file, sheet = "Ages", header = 9)		
+	}
 
 	find = names(hauls) == "N_Tow_Tally_(Stratum)"
 	names(hauls[find]) = "N_Tow_Tally"
 
 	# Currently not working below here
+	# The default column names with () are not being read by the aggragate function
 	xx = aggregate(N.Tow.Tally..Stratum. ~ Survey.Year, sum, data = hauls)
 	nSamp = xx[,2]
 	n = floor(n.unq * nSamp) 
@@ -41,5 +52,6 @@ getN <- function(dir, file, headerRow, species = "flatfish", printfolder = "forS
     if(is.na(plotdir.isdir) | !plotdir.isdir){
       dir.create(plotdir)
     }
-	write.csv(samples, file = paste0(plotdir, "/NWFSC_TW_Survey_LengthSampleSize.csv")
+	write.csv(samples, file = paste0(plotdir, "/NWFSC_TW_Survey_", type, "_SampleSize.csv"))
+	return(n)
 }
