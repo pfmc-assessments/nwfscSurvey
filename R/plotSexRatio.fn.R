73f2187773174cda37a2520724cabf2e1d7637f7
Chantel Wetzel <chantel.wetzel@noaa.gov>
2018-05-09 08:24:38 -0700

    Modification to the sex ratio plot function
    Now works with the triennial data set

--- R/plotSexRatio.fn.R
+++ R/plotSexRatio.fn.R
@@ -1,18 +1,46 @@
 #' Function to plot sex ratio
 #'
-#' @param len data object
+#' @param dir directory location for saving the png
+#' @param dat data object
 #' @param fn value to calculate from the data (e.g., median, mean)
+#' @param survey specify the survey data set for calculations. Options are "NWFSCBT", "Tri.Shelf", "AFSC.Slope"
 #' @param circleSize circle size 
+#' @param dopng TRUE/FALSE whether to save a png file
 #'
-#' @author Allan Hicks 
+#' @author Allan Hicks and Chantel Wetzel
 #' @export
 
-plotSexRatio.fn <-function(len, fn=median, circleSize=0.1,...) {
-    ratioF <- len$NumF/(len$NumF+len$NumM)
-    yF <- lapply(split(ratioF,floor(len$Length)),fn,na.rm=TRUE)
-    x <- names(split(ratioF,floor(len$Length)))
-    nobs <- unlist(lapply(split(ratioF,floor(len$Length)),length))
-    plot(x,yF,type="l",col="red",xlab="Length (cm)",ylab="Fraction female",...)
-    symbols(x,yF,circles=nobs,inches=circleSize,fg="red",bg=rgb(1,0,0,alpha=0.5),add=T)
-    return(invisible(data.frame(length=x,fraction.female=as.numeric(yF))))
+plotSexRatio.fn <-function(dir, dat, fn=median, survey, circleSize=0.1, dopng = FALSE,...) {
+
+    plotdir <- paste0(dir, "/plots")
+    plotdir.isdir <- file.info(plotdir)$isdir
+    if(is.na(plotdir.isdir) | !plotdir.isdir){
+      dir.create(plotdir)
+    }
+
+    
+	if (survey == "NWFSCBT") {
+		if (dopng) { png(paste0(dir, "/plots/", survey, "_fraction_female.png"), height=7, width=7, units="in",res=300) }
+    	ratioF <- dat$NumF/(dat$NumF+dat$NumM)
+    	yF <- lapply(split(ratioF,floor(dat$Length)),fn,na.rm=TRUE)
+    	x <- names(split(ratioF,floor(dat$Length)))
+    	nobs <- unlist(lapply(split(ratioF,floor(len$Length)),length))
+    	plot(x,yF,type="l",col="red",xlab="Length (cm)",ylab="Fraction female",...)
+    	symbols(x,yF,circles=nobs,inches=circleSize,fg="red",bg=rgb(1,0,0,alpha=0.5),add=T)
+    	return(invisible(data.frame(length=x,fraction.female=as.numeric(yF))))
+    }
+
+    if (survey == "Tri.Shelf"){
+    	axis.name = ifelse(is.null(dat$AGE), "Length (cm)", "Age")
+    	data.type = ifelse(is.null(dat$AGE), "length", "age")
+    	if (dopng) { png(paste0(dir, "/plots/", survey, "_fraction_female_by_",data.type,".png"), height=7, width=7, units="in",res=300) }
+    	if (data.type == "length") { temp =  table(dat$Length_cm, dat$SEX) }
+    	if (data.type == "age")    { temp =  table(dat$AGE, dat$SEX) }
+    	ratioF = temp[,2] / (temp[,1] + temp[,2])
+    	nobs = temp[,1] + temp[,2]
+    	plot(ratioF,type="l", col="red", xlab=axis.name, ylab="Fraction female",...)
+    	symbols(ratioF,circles=nobs,inches=circleSize,fg="red",bg=rgb(1,0,0,alpha=0.5),add=T)
+    }
+    if (dopng) { dev.off()}
+
 }
