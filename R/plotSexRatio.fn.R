534134faf4ceb798280a7822de18db183fbc34b6
Chantel Wetzel <chantel.wetzel@noaa.gov>
2018-08-08 08:57:53 -0700

    Correction to the sex ratio plot

--- R/plotSexRatio.fn.R
+++ R/plotSexRatio.fn.R
@@ -2,6 +2,7 @@
 #'
 #' @param dir directory location for saving the png
 #' @param dat data object
+#' @param data.type "length" or "age" 
 #' @param survey specify the survey data set for calculations. Options are "NWFSCBT", "Tri.Shelf", "AFSC.Slope"
 #' @param circleSize circle size 
 #' @param dopng TRUE/FALSE whether to save a png file
@@ -10,7 +11,7 @@
 #' @author Allan Hicks and Chantel Wetzel
 #' @export
 
-PlotSexRatio.fn <-function(dir, dat, type = "length",  survey, circleSize=0.1, dopng = FALSE,...) {
+PlotSexRatio.fn <-function(dir, dat, data.type = "length",  survey, circleSize=0.1, dopng = FALSE,...) {
 
     plotdir <- paste0(dir, "/plots")
     plotdir.isdir <- file.info(plotdir)$isdir
@@ -18,15 +19,15 @@ PlotSexRatio.fn <-function(dir, dat, type = "length",  survey, circleSize=0.1, d
       dir.create(plotdir)
     }
     
-	if (dopng) { png(paste0(dir, "/plots/", survey, "_fraction_female.png"), height=7, width=7, units="in",res=300) }
+	if (dopng) { png(paste0(dir, "/plots/", survey, "_", data.type, "_fraction_female.png"), height=7, width=7, units="in",res=300) }
 
     if (data.type == "length") { temp =  table(dat$Length_cm, dat$Sex); axis.name = "Length (cm)" }
     if (data.type == "age")    { temp =  table(dat$Age, dat$Sex); axis.name = "Age" }
 
     ratioF = temp[,"F"] / (temp[,"M"] + temp[,"F"])
     nobs = temp[,"F"] + temp[,"M"]
-    plot(ratioF,type="l", col="red", xlab=axis.name, ylab="Fraction female",...)
-    symbols(ratioF,circles=nobs,inches=circleSize,fg="red",bg=rgb(1,0,0,alpha=0.5),add=T)
+    plot(x = names(ratioF), y = ratioF, type="l", col="red", xlab=axis.name, ylab="Fraction female",...)
+    symbols(x = names(ratioF), y = ratioF, circles=nobs, inches=circleSize, fg="red", bg=rgb(1,0,0,alpha=0.5), add=T)
 
     if (dopng) { dev.off()}
 
