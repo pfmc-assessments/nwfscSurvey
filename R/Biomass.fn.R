0b6d02be708a5163e2eea2bade86a227cd776580
Sean Anderson <sean@seananderson.ca>
2019-03-29 20:17:18 -0700

    Add imports

--- R/Biomass.fn.R
+++ R/Biomass.fn.R
@@ -1,30 +1,34 @@
-#' Calculates design based estimates from survey data for West Coast surveys 
+#' Calculates design based estimates from survey data for West Coast surveys
 #'
 #' The variables defining the strata must begin with the name in strat.vars and end with ".1" or ".2" (i.e., Depth_m.1)
 #' the strata are assumed to be continuous variables, thus have a lower and upper value defining them. The lower value does not necessarily have to be the same as the previous upper value.
 #' the stat.df dataframe is difficult to build up with more than one variable becuase it turns into a design where you have to define all areas, thus repeat the variables for one (like a design)
-#' 
+#'
 #' I calculate the variance given stratified sampling theory
 #' I work in normal space, then calculate the statistics if B is lognormal
 #' This is the Mean Ratio Estimate
-#' 
+#'
 #' @param dir directory where the output file will be saved
 #' @param dat data-frame of the data that has been by the PullCatch.fn
 #' @param stat.vars A vector of the strata variable names (i.e., c("Depth_m","Latitude_dd"))
 #' @param strat.df a dataframe with the first column the name of the stratum, the second column the area of the stratum, and the remaining columns are the high and low variables defining the strata created by the CreateStrataDF.fn
 #' @param printfolder the folder where files will be saved
 #' @param outputMedian T/F output median or the mean biomass estimate
-#' @param convert convertion factor for the biomass by area units (e.g., hectare vs km2) 
+#' @param convert convertion factor for the biomass by area units (e.g., hectare vs km2)
 #' @param month month for SS
 #' @param fleet fleet number for SS
 #' @param verbose opt to print out message statements
 #'
 #' @author Allan Hicks and Chantel Wetzel
+#' @importFrom grDevices dev.off png rgb
+#' @importFrom graphics axis legend mtext par plot points segments symbols
+#' @importFrom stats optim qnorm sd var
+#' @importFrom utils write.csv
 #' @export
 
-Biomass.fn <- function(dir = NULL, dat, strat.vars = c("Depth_m","Latitude_dd"), strat.df, printfolder = "forSS", outputMedian = T, 
+Biomass.fn <- function(dir = NULL, dat, strat.vars = c("Depth_m","Latitude_dd"), strat.df, printfolder = "forSS", outputMedian = T,
                        convert = 1, month = NA, fleet = NA, verbose = TRUE)  {
-    
+
 
     if(is.null(dat$cpue_kg_km2)) stop("There must be a column called cpue_kg_km2 in the dataframe")
     # Calculate the CPUE in terms of nubmer
@@ -33,7 +37,7 @@ Biomass.fn <- function(dir = NULL, dat, strat.vars = c("Depth_m","Latitude_dd"),
     row.names(strat.df) <- strat.df[,1]     #put in rownmaes to make easier to index later
     numStrata <- nrow(strat.df)
 
-    numStrata <- nrow(strat.df)   
+    numStrata <- nrow(strat.df)
     #create strata factors
     stratum <- rep(NA,nrow(dat))        #the stratum factor
     for(strat in 1:numStrata) {
@@ -44,7 +48,7 @@ Biomass.fn <- function(dir = NULL, dat, strat.vars = c("Depth_m","Latitude_dd"),
         stratum[ind] <- as.character(strat.df[strat,1])
     }
     stratum <- factor(stratum,levels=as.character(strat.df[,1]))
-    dat <- data.frame(dat, stratum)  
+    dat <- data.frame(dat, stratum)
     dat.yr <- split(dat,dat$Year)
     dat.stratum <- split(dat, dat$stratum)
 
@@ -62,7 +66,7 @@ Biomass.fn <- function(dir = NULL, dat, strat.vars = c("Depth_m","Latitude_dd"),
         meanNumbersInStrat    <- unlist(lapply(x, function(x) {mean(x$cpue_km2_count)}))
         varNumbersInStrat     <- unlist(lapply(x, function(x) {var (x$cpue_km2_count)}))
 
-        stratStats <- data.frame(name = namesStrat, area = strat.df[namesStrat,2], ntows = nobs, 
+        stratStats <- data.frame(name = namesStrat, area = strat.df[namesStrat,2], ntows = nobs,
                                  meanCatchRate = meanCatchRateInStrata, varCatchRate = varCatchRateInStrata)
 
         stratStats$Bhat <- stratStats$area*stratStats$meanCatchRate
@@ -90,7 +94,7 @@ Biomass.fn <- function(dir = NULL, dat, strat.vars = c("Depth_m","Latitude_dd"),
         meanNumbersInStrat    <- unlist(lapply(x, function(x) {mean(x$cpue_km2_count)}))
         varNumbersInStrat     <- unlist(lapply(x, function(x) {var (x$cpue_km2_count)}))
 
-        stratStats <- data.frame(name = namesYear, area = strat.df[ind,2], ntows = nobs, 
+        stratStats <- data.frame(name = namesYear, area = strat.df[ind,2], ntows = nobs,
                                  meanCatchRate = meanCatchRateInStrata, varCatchRate = varCatchRateInStrata)
 
         stratStats$Bhat <- stratStats$area*stratStats$meanCatchRate
@@ -108,19 +112,19 @@ Biomass.fn <- function(dir = NULL, dat, strat.vars = c("Depth_m","Latitude_dd"),
     names(yearlyStrataEsts) <- paste("Year",names(yearlyStrataEsts),sep="")
 
     stratumEsts <- lapply(dat.stratum, strata.fn)
-    
+
     yrTotal.fn <- function(x) {
         data.frame(Bhat=sum(x$Bhat),seBhat=sqrt(sum(x$varBhat)),cv=sqrt(sum(x$varBhat))/sum(x$Bhat))
     }
 
     ests <- as.data.frame(t(as.data.frame(lapply(lapply(yearlyStrataEsts, yrTotal.fn),t)))) #some crazy stuff to put into a dataframe with years as rows
     logVar <- log(ests$cv^2+1)
-    ln <- data.frame(year = substring(row.names(ests),5), 
+    ln <- data.frame(year = substring(row.names(ests),5),
                      meanBhat = ests$Bhat/convert,
                      medianBhat = ests$Bhat*exp(-0.5*logVar)/convert,
                      SElogBhat = sqrt(logVar))
 
-    
+
     df.list = list()
     df  <- list(Strata = yearlyStrataEsts, Total = ests, LNtons = ln)
     if(outputMedian) { bio <- data.frame(Year = df$LNtons$year, Season = month, Fleet = fleet, Value = df$LNtons$medianBhat, seLogB = df$LNtons$SElogBhat) }
