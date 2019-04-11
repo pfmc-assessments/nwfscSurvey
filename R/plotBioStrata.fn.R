e1c066301912dc334f0f1b683bc43962e309e101
Chantel Wetzel <chantel.wetzel@noaa.gov>
2019-04-11 15:40:01 -0700

    Fix main input where it will save as the survey name

--- R/plotBioStrata.fn.R
+++ R/plotBioStrata.fn.R
@@ -26,8 +26,8 @@ PlotBioStrata.fn <-function(dir = NULL, dat, CI=0.95, scalar=1e6, gap=0.03, ylab
     strata.names = names(bio)
 
     if (dopng) {
-      #plotdir <- paste0(dir, "/plots")
       if(is.null(dir)){stop("Directory needs to be set.")}
+      if (!file.exists(dir)) { stop("The dir argument leads to a location", ",\ni.e., ", dir, ", that doesn't exist.") }
       plotdir <- file.path(dir, paste("plots", sep=""))
       plotdir.isdir <- file.info(plotdir)$isdir
       if(is.na(plotdir.isdir) | !plotdir.isdir){
