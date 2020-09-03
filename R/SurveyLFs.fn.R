aa32f6fd84dfcac9b32659537a1226293b781aab
Chantel Wetzel <chantel.wetzel@noaa.gov>
2020-09-03 08:28:06 -0700

    fix: changes to function documentation

--- R/SurveyLFs.fn.R
+++ R/SurveyLFs.fn.R
@@ -26,7 +26,7 @@
 #' @param month month the samples were collected
 #' @param printfolder folder where the length comps will be saved
 #' @param remove999 the output object by the function will have the 999 column combined with the first length bin
-#' @param outputStage1 return the first stage expanded data without compiling it for SS
+#' @param outputStage1 TRUE/FALSE return the first stage expanded data without compiling it for SS
 #' @param verbose opt to print out message statements
 #'
 #' @author Allan Hicks and Chantel Wetzel
