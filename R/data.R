19db384e1edbd0913b7fb16d84ecb9f3f3b7421b
kellijohnson-NOAA <kelli.johnson@nooa.gov>
2021-05-04 15:42:41 -0700

    availablecells for survey from 2018 as data object

--- R/data.R
+++ R/data.R
@@ -64,3 +64,22 @@
 #' @seealso \code{\link{PullSpp.fn}} for more information.
 #'
 "PullSpp"
+
+#' Available grid cell locations for the survey
+#'
+#' Available grid cell locations that were used to choose the
+#' 2018 survey locations for the WCGBTS. Cells are a truncated
+#' set compared to the full set because of closures to trawlable
+#' areas such as what would be closed because of a new
+#' management measure.
+#' In the future, this table should be available from the
+#' data warehouse itself rather than a static snapshot from
+#' a single year.
+#'
+#' @author Eric Ward
+#'
+#' @format A data frame with fourteen columns,
+#' Cent.ID, Depth.Range, Lat.34.5, Hectares, Cent.Lat, Cent.Long,
+#' SE.LON, SW.LAT, NW.LAT, NE.LON, NE.LAT, SE.LON, SE.LAT
+#'
+"availablecells"
---
+++ data-raw/FRAM_DW_grid.R
@@ -0,0 +1,10 @@
+#### Create a data frame of available survey cells
+# @Curt-Whitmire-NOAA is working on providing this table within the data warehouse
+# itself with the additional data of year of decommissioning if a cell is
+# no longer in the lottery for future years
+
+availablecells <- readxl::read_excel(
+  path = dir(pattern = "Selection Set 2018 with Cell Corners", recursive = TRUE)
+  ) %>% data.frame
+
+usethis::use_data(availablecells, overwrite = TRUE)
