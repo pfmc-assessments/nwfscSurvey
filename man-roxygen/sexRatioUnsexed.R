7ee1baa1f95688fa50cea17ba84da0239592f2e2
Chantel Wetzel <chantelwetzel@noaa.gov>
2022-08-10 09:02:05 -0700

    refactor: update format

--- man-roxygen/sexRatioUnsexed.R
+++ man-roxygen/sexRatioUnsexed.R
@@ -1,2 +1,2 @@
 #' @param sexRatioUnsexed Sex ratio to apply to any length bins of a 
-#' certain size or smaller as defined by the maxSizeUnsexed. The default is `NA`.
+#'   certain size or smaller as defined by the maxSizeUnsexed. The default is `NA`.
