f0159b9e61641c50fb451540a0f915073dfeda0d
Chantel Wetzel <chantelwetzel@noaa.gov>
2022-08-24 10:16:11 -0700

    add: templates for pull function inputs

---
+++ man-roxygen/common_name.R
@@ -0,0 +1,6 @@
+#' @param common_name A character entry with the desired common name of the
+#' species you want to pull data for from the data warehouse.
+#' Use a vector of names if you want information for more than one species or
+#' if the desired species is included in the database using more than one name,
+#' e.g., vermilion rockfish (see the example below).
+#' Use the `sci_name` argument if you know the latin name.
---
+++ man-roxygen/years.R
@@ -0,0 +1,2 @@
+#' @param years An integer vector of length two with the
+#' range of years to pull data for.
