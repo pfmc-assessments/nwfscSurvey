f94ae7e29b7bcd230c1c2f31cae8d99ea7751715
Chantel Wetzel <chantelwetzel@noaa.gov>
2022-11-18 09:07:31 -0800

    add: new plotting fxns for comps, cpue, and bio patterns

---
+++ R/plot_bio_patterns.R
@@ -0,0 +1,153 @@
+#' This function plots length by latitude and depth
+#'
+#' @param dir Directory to save files to
+#' @param bio Data biological sample file
+#' @param col_name Option to switch between plotting lengths or ages. 
+#' Options are "Length_cm", "Width_cm", or "Age".
+#' @param width Numeric figure width in inches, defaults to 7
+#' @param height Numeric figure height in inches, defaults to 7
+#'
+#' @import ggplot2
+#' @import cowplot
+#' 
+#' @author Chantel Wetzel
+#' @export
+#' 
+#' 
+plot_bio_patterns <- function(
+  dir = NULL, 
+  bio, 
+  col_name = "Length_cm",
+  plot = 1:3, 
+  width = 7,
+  height = 7){
+ 
+  # plot 1 = length/age by latitude and depth
+  # plot 2 = length/age by depth and year
+  # plot 3 = length/age by lat and year
+
+  round_any <- function(x, accuracy, f = round) {
+      f(x / accuracy) * accuracy
+  }
+
+  if (!is.null(dir)){
+    if (!file.exists(dir)) {
+      stop("The dir argument leads to a location", ",\ni.e., ", dir, ", that doesn't exist.")
+    }  
+    plotdir <- file.path(dir, "plots")
+    plotdir.isdir <- file.info(plotdir)$isdir
+    if (is.na(plotdir.isdir) | !plotdir.isdir) {
+      dir.create(plotdir)
+    }
+  }
+
+  lab_name = col_name
+  if (col_name == "Length_cm"){
+    lab_name = "Length (cm)"
+    bio$x <- bio$Length_cm
+  }
+  if (col_name == "Width_cm"){
+    lab_name = "Width (cm)"
+    bio$x = bio$Width_cm
+  }
+  if (col_name == "Age"){
+    lab_name = "Age (yrs)"
+    bio$x <- bio$Age
+  }
+
+  alpha_set <- 0.30
+  if (dim(bio)[1] > 10000){
+    alpha_set <- 0.05
+    alpha_by_year <- 0.20
+    if (dim(bio)[1] > 50000){
+      alpha_set <- 0.01
+      alpha_by_year <- 0.10
+    }
+  }
+  bin_size <- ifelse(max(bio$Depth_m) - min(bio$Depth_m) > 500, 100, 
+    ifelse(max(bio$Depth_m) - min(bio$Depth_m) > 250, 50, 25))
+  bio$depth_bin <- round_any(bio$Depth_m, bin_size, f = floor)
+  bio$lat <- round_any(bio$Latitude_dd, 0.25)
+
+  # Length by depth for each sex
+  ld <- ggplot2::ggplot(bio, aes(x = Depth_m, y = x, color = Sex)) +
+      geom_point(aes(fill = Sex, colour = Sex), alpha = alpha_set, shape = 21, size = 3) +
+      stat_summary(aes(y = x, x = depth_bin), fun = mean, geom = 'line', 
+        lwd = 2, alpha = 1) + 
+      scale_fill_manual(values = c('F' = 'red', 'M' = 'blue', 'U' = "darkseagreen")) +
+      scale_color_manual(values = c('F' = 'darkred', 'M' = 'darkblue', 'U' = "darkgreen")) +
+      labs(x = "Depth (m)", y = lab_name) +
+      scale_x_continuous(n.breaks = 7) +
+      scale_y_continuous(n.breaks = 7) +
+      theme(legend.key = element_blank(), 
+        axis.text.x = element_text(colour = "black", size = 12), 
+        axis.text.y = element_text(colour = "black", size = 11), 
+        legend.text = element_text(size = 10, colour ="black"), 
+        legend.title = element_text(size = 12), 
+        panel.background = element_blank(), 
+        panel.border = element_rect(fill = NA), 
+        legend.position = "right")
+
+  # Length by latitude    
+  ll <- ggplot2::ggplot(bio, aes(x = Latitude_dd, y = x)) +
+    geom_point(aes(fill = Sex, colour = Sex), alpha = alpha_set, shape = 21, size = 3) +
+    stat_summary(aes(y = x, x = lat, colour = Sex), fun = mean, geom = 'line', 
+      lwd = 2, alpha = 1) + 
+    scale_fill_manual(values = c('F' = 'red', 'M' = 'blue', 'U' = "darkseagreen")) +
+    scale_color_manual(values = c('F' = 'darkred', 'M' = 'darkblue', 'U' = "darkgreen")) +
+    labs(x = "Latitude", y = lab_name) +
+    scale_x_continuous(n.breaks = 7) +
+    scale_y_continuous(n.breaks = 7) +
+    theme(legend.key = element_blank(), 
+      axis.text.x = element_text(colour = "black", size = 12), 
+      axis.text.y = element_text(colour = "black", size = 11), 
+      legend.text = element_text(size = 10, colour ="black"), 
+      legend.title = element_text(size = 12), 
+      panel.background = element_blank(), 
+      panel.border = element_rect(fill = NA), 
+      legend.position = "right")
+
+  # Length by latitude and sex by year
+  lly <- ggplot2::ggplot(bio, aes(x = Latitude_dd, y = x)) +
+    geom_point(aes(fill = Sex, colour = Sex), 
+      alpha = alpha_by_year, shape = 21, size = 3) +
+    facet_wrap(facets = "Year") + 
+    scale_fill_manual(values = c('F' = 'red', 'M' = 'blue', 'U' = "darkseagreen")) +
+    scale_color_manual(values = c('F' = 'darkred', 'M' = 'darkblue', 'U' = "darkgreen")) +
+    labs(x = "Latitude", y = lab_name) + 
+    theme(panel.background = element_blank(), 
+          panel.border = element_rect(fill = NA))
+
+  # Length by depth and sex by year
+  ldy <- ggplot2::ggplot(bio, aes(x = Depth_m, y = x)) +
+    geom_point(aes(fill = Sex, colour = Sex), 
+      alpha = alpha_by_year, shape = 21, size = 3) +
+    facet_wrap(facets = "Year") + 
+    scale_fill_manual(values = c('F' = 'red', 'M' = 'blue', 'U' = "darkseagreen")) +
+    scale_color_manual(values = c('F' = 'darkred', 'M' = 'darkblue', 'U' = "darkgreen")) +
+    labs(x = "Depth (m)", y = lab_name) + 
+    theme(panel.background = element_blank(), 
+          panel.border = element_rect(fill = NA))
+
+  # plot 1
+  cowplot::plot_grid(ll, ld, nrow = 2) 
+  if(!is.null(dir)){ 
+      ggsave(filename = file.path(dir, "plots", paste0(col_name, "_by_lat_depth.png")), 
+          width = width, height = height, units = 'in') 
+  }
+
+  # plot 2
+  print(ldy)
+  if(!is.null(dir)){ 
+      ggsave(filename = file.path(dir, "plots", paste0(col_name, "_by_year_depth.png")), 
+          width = width + 3, height = height + 3, units = 'in') 
+  }   
+
+  # plot 3
+  print(lly)
+  if(!is.null(dir)){ 
+      ggsave(filename = file.path(dir, "plots", paste0(col_name, "_by_year_lat.png")), 
+          width = width + 3, height = height + 3, units = 'in') 
+  }  
+ 
+}
---
+++ R/plot_cpue.R
@@ -0,0 +1,116 @@
+#' This function plots cpue and length by latitude and depth
+#'
+#' @param dir Directory to save files to
+#' @param catch Data catch file
+#' @param add_save_name Option to add text to a saved figure name. This option can
+#' be useful if creating plots across multiple species and saving them into a single
+#' folder. Default NULL.
+#' @param width Numeric figure width in inches, defaults to 7
+#' @param height Numeric figure height in inches, defaults to 7
+#'
+#' @import ggplot2
+#' @import cowplot
+#' 
+#' @author Chantel Wetzel
+#' @export
+#' 
+plot_cpue <- function(
+  dir = NULL, 
+  catch, 
+  plot = 1:3, 
+  width = 7,
+  height = 7){
+
+  # plot 1 = cpue by latitude and depth 
+  # plot 2 = cpue by lat and year
+  # plot 3 = cpue by depth and year
+
+  if (!is.null(dir)){
+    if (!file.exists(dir)) {
+      stop("The dir argument leads to a location", ",\ni.e., ", dir, ", that doesn't exist.")
+    }  
+    plotdir <- file.path(dir, "plots")
+    plotdir.isdir <- file.info(plotdir)$isdir
+    if (is.na(plotdir.isdir) | !plotdir.isdir) {
+      dir.create(plotdir)
+    }
+  }
+
+  catch$log_cpue <- log(catch$cpue_kg_km2)
+  pos <- catch$cpue_kg_km2 != 0
+  size_adj <- 100 / floor(sum(pos))
+
+  # log(cpue) by depth in meters
+  cd <- ggplot2::ggplot(catch[pos,], aes(x = Depth_m, y = log_cpue)) +
+        geom_point(aes(size = log_cpue / size_adj), fill = 'darkorange', colour = 'darkorange', alpha = 0.75, shape = 21) +
+        labs(x = "Depth (m)", y = "ln(CPUE)", size = "ln(CPUE)", fill = 'darkorange') +
+        geom_smooth(method = 'loess', color = 'darkgrey', lwd = 2) + 
+        scale_x_continuous(n.breaks = 7) +
+        scale_y_continuous(n.breaks = 7) +
+        theme(legend.key = element_blank(), 
+          axis.text.x = element_text(colour = "black", size = 12), 
+          axis.text.y = element_text(colour = "black", size = 11), 
+          legend.text = element_text(size = 10, colour ="black"), 
+          legend.title = element_text(size = 12), 
+          panel.background = element_blank(), 
+          panel.border = element_rect(fill = NA), 
+          legend.position = "right") +
+        guides(size = "legend", color = "none", fill = "none")
+
+  # log(cpue) by latitude
+  cl <- ggplot2::ggplot(catch[pos,], aes(x = Latitude_dd, y = log_cpue)) +
+        geom_point(aes(size = log_cpue / size_adj), fill = 'darkorange', colour = 'darkorange', alpha = 0.75, shape = 21) +
+        geom_smooth(method = 'loess', color = 'darkgrey', lwd = 2) +  
+        labs(x = "Latitude", y = "ln(CPUE)", size = "ln(CPUE)", fill = 'darkorange', colour = 'darkorange',) +
+        scale_x_continuous(n.breaks = 7) +
+        scale_y_continuous(n.breaks = 7) +
+        theme(legend.key = element_blank(), 
+          axis.text.x = element_text(colour = "black", size = 12), 
+          axis.text.y = element_text(colour = "black", size = 11), 
+          legend.text = element_text(size = 10, colour ="black"), 
+          legend.title = element_text(size = 12), 
+          panel.background = element_blank(), 
+          panel.border = element_rect(fill = NA), 
+          legend.position = "right") 
+
+  cly <- ggplot2::ggplot(catch[pos,], aes(x = Latitude_dd, y = log_cpue)) +
+    geom_point(aes(size = log_cpue / (100*size_adj)), fill = 'darkorange', colour = 'darkorange', alpha = 0.75, shape = 21) +
+    facet_wrap(facets = "Year") + 
+    geom_smooth(method = 'loess', color = 'darkgrey', lwd = 2) + 
+    labs(x = "Latitude", y = "ln(CPUE)", size = "ln(CPUE)", fill = 'darkorange', colour = 'darkorange',) +
+    guides(size = "legend", color = "none", fill = "none") + 
+    theme(panel.background = element_blank(), 
+          panel.border = element_rect(fill = NA))
+
+  # Length by depth and sex by year
+  cdy <- ggplot2::ggplot(catch[pos, ], aes(x = Depth_m, y = log_cpue)) +
+    geom_point(aes(size = log_cpue / (100*size_adj)), fill = 'darkorange', colour = 'darkorange', alpha = 0.75, shape = 21) +
+    facet_wrap(facets = "Year") + 
+    geom_smooth(method = 'loess', color = 'darkgrey', lwd = 2) + 
+    labs(x = "Depth (m)", y = "ln(CPUE)", size = "ln(CPUE)", fill = 'darkorange', colour = 'darkorange') +
+    guides(size = "legend", color = "none", fill = "none") + 
+    theme(panel.background = element_blank(), 
+          panel.border = element_rect(fill = NA))
+
+  # plot 1
+  cowplot::plot_grid(cl, cd, nrow = 2)
+  if(!is.null(dir)){    
+    ggsave(filename = file.path(dir, "plots", "cpue_by_lat_depth.png"), 
+      width = width, height = height, units = 'in')     
+  }  
+
+  # plot 2
+  print(cly)
+  if(!is.null(dir)){ 
+      ggsave(filename = file.path(dir, "plots", "cpue_by_year_lat.png"), 
+          width = width + 3, height = height + 3, units = 'in') 
+  } 
+
+  # plot 3  
+  print(cdy)
+  if(!is.null(dir)){ 
+      ggsave(filename = file.path(dir, "plots", "cpue_by_year_depth.png"), 
+          width = width + 3, height = height + 3, units = 'in') 
+  } 
+ 
+}
