0d74b4149b590ee271e9a0f1d93e12fe65d01639
Chantel Wetzel <chantelwetzel@noaa.gov>
2022-11-21 16:36:42 -0800

    refactor: update the presence/absence and sex ratio figures

--- R/PlotPresenceAbsence.fn.R
+++ R/PlotPresenceAbsence.fn.R
@@ -1,14 +1,20 @@
 #' Plot showing presence and absence per haul by depth or latitute bin
 #'
-#' Uses the data.frame of catch values extracted from the data warehouse
-#' by [PullCatch.fn()] to make a table of zero and non-zero catch by
-#' depth or latitute bins.
+#' Uses the data.frame of data values extracted from the data warehouse
+#' by either [PullCatch.fn()] or [PullBio.fn()] to make a table of 
+#' proportion by depth or latitude bins. If biological data are passed
+#' to the function then there is a single option to plot the sex 
+#' ratio by depth.
 #'
-#' @param catch data.frame containing catch per haul created by
-#' PullCatch.fn()
-#' @param dim Dimension of interest, either "depth" or "lat"
+#' @param data data.frame containing data per haul created by
+#' PullCatch.fn() or biological data created by PullBio.fn() where the
+#' dim input must be "sex". 
+#' @param dim Dimension of interest, either "depth", "lat", or "sex"
 #' @param dir The directory name to print to
-#' @param dopng Boolean, whether or not to do png. Defaults to FALSE,
+#' @param plot_type Two options area avialable "proportion" or "total" where
+#' the default, "proportion", plot the proportion by depth/latitude with equal
+#' bar widths and the "total" option plot the numbers by depth/latitude were
+#' the bar widths in a production of sampling by depth/latitude.
 #' @param depth_min Minimum depth (in meters)
 #' @param depth_max Maximum depth (in meters). A NULL value will cause the
 #' function to automatically set depth_max to the multiple of depth_bin_width beyond
@@ -19,115 +25,50 @@
 #' @param lat_bin_width Width of each latitude bin (in decimal degrees)
 #' @param add_range_to_main Add the range of latitude or depth by which the data
 #' are filtered
-#' @param main String title, defaults to "Presence/absence in WCGBT Survey"
 #' @param xlab Label for x-axis. A NULL value will cause the function
 #' to choose either "Depth (m)" or "Latitude (°N)"
 #'
-#' @author Ian G. Taylor
+#' @author Ian G. Taylor and Chantel Wetzel
 #' @importFrom grDevices gray
 #' @export
 #' @examples
-#' # load WCGBTS catch data
-#' catch.WCGBTS.ling <- nwfscSurvey::PullCatch.fn(
+#' # load WCGBTS data data
+#' data.WCGBTS.ling <- nwfscSurvey::PullCatch.fn(
 #'   Name = "lingcod",
 #'   SurveyName = "NWFSC.Combo"
 #' )
-#' PlotPresenceAbsence.fn(catch.WCGBTS.ling, dim = "lat")
-#' PlotPresenceAbsence.fn(catch.WCGBTS.ling, dim = "depth")
-PlotPresenceAbsence.fn <- function(catch,
-                                   dim = "depth",
+#' bio.WCGBTS.ling <- nwfscSurvey::PullBio.fn(
+#'   Name = "lingcod",
+#'   SurveyName = "NWFSC.Combo"
+#' )
+#' PlotPresenceAbsence.fn(data = data.WCGBTS.ling, dim = "lat")
+#' PlotPresenceAbsence.fn(data = data.WCGBTS.ling, dim = "depth")
+#' PlotPresenceAbsence.fn(data = bio.WCGBTS.ling, dim = "sex")
+#' 
+PlotPresenceAbsence.fn <- function(data,
+                                   dim = c("depth", "lat", "sex")[1],
                                    dir = NULL,
-                                   dopng = FALSE,
+                                   plot_type = c('proportion', 'total')[1],
                                    depth_min = 50,
                                    depth_max = NULL,
                                    depth_bin_width = 25,
                                    lat_min = 32,
                                    lat_max = 49,
                                    lat_bin_width = 1.0,
-                                   main = "Presence/absence in WCGBT Survey",
                                    add_range_to_main = TRUE,
                                    xlab = NULL) {
-  # check inputs
-  if (!dim %in% c("depth", "lat")) {
-  }
 
-  # set depth_max if not provided
-  if (is.null(depth_max)) {
-    depth_999 <- quantile(
-      prob = 0.999,
-      x = catch$Depth_m[catch$Latitude_dd > lat_min &
-        catch$Latitude_dd < lat_max &
-        catch$cpue_kg_km2 > 0]
-    )
-    depth_max <- 2 * depth_bin_width + depth_bin_width * floor(depth_999 / depth_bin_width)
-    message(
-      "99.9% of positive hauls are shallower than ", round(depth_999), ".\n",
-      "'depth_max' set to ", depth_max, ". Input alternative value if you wish."
-    )
-  }
+  plot_proportion(data = data,
+    dim = dim,
+    dir = dir,
+    plot_type = plot_type,
+    depth_min = depth_min,
+    depth_max = depth_max,
+    depth_bin_width = depth_bin_width,
+    lat_min = lat_min,
+    lat_max = lat_max,
+    lat_bin_width = lat_bin_width,
+    add_range_to_main = add_range_to_main,
+    xlab = xlab)
 
-  # filter catch based on input depth and latitute range
-  catch2 <- catch[catch$Depth_m < depth_max &
-    catch$Depth_m > depth_min &
-    catch$Latitude_dd > lat_min &
-    catch$Latitude_dd < lat_max, ]
-
-  # note: \u00B0 is degree symbol and \u2013 is n-dash
-
-  # depth as dimension of interest
-  if (dim == "depth") {
-    # get binned value
-    binned_value <- depth_bin_width * floor(catch2$Depth_m / depth_bin_width)
-    # update xlab and caption
-    if (is.null(xlab)) {
-      xlab <- "Depth (m)"
-    }
-    if (add_range_to_main) {
-      main <- paste0(main, " (", lat_min, "\u00B0N \u2013 ", lat_max, "\u00B0N)")
-    }
-    filename <- "Presence-absence_by_depth_in_WCGBT_Survey.png"
-  }
-  # rounded value for latitude
-  if (dim == "lat") {
-    # get binned value
-    binned_value <- lat_bin_width * floor(catch2$Latitude_dd / lat_bin_width)
-    # update xlab and caption
-    if (is.null(xlab)) {
-      xlab <- "Latitude (\u00B0N)"
-    }
-    if (add_range_to_main) {
-      main <- paste0(main, " (", depth_min, " \u2013 ", depth_max, "m)")
-    }
-    filename <- "Presence-absence_by_latitude_in_WCGBT_Survey.png"
-  }
-
-  # code in "if (dopng)" section below copied from plotBio.fn()
-  if (dopng) {
-    if (is.null(dir)) {
-      stop("Directory needs to be set.")
-    }
-    if (!file.exists(dir)) {
-      stop("The dir argument leads to a location", ",\ni.e., ", dir, ", that doesn't exist.")
-    }
-    plotdir <- file.path(dir, paste("plots", sep = ""))
-    plotdir.isdir <- file.info(plotdir)$isdir
-    if (is.na(plotdir.isdir) | !plotdir.isdir) {
-      dir.create(plotdir)
-    }
-    png(file.path(dir, "plots", filename),
-      height = 7, width = 7, units = "in", res = 300
-    )
-  }
-
-  # make table
-  tab <- table(binned_value, catch2$cpue_kg_km2 > 0)
-  # add names for the dimensions which get used in the plot
-  dimnames(tab)[[2]] <- c("Absent", "Present")
-  # make plot
-  plot(tab, col = c(gray(.7), "blue3"), cex = 1, main = main, xlab = xlab)
-
-  # close PNG if it was used
-  if (dopng) {
-    dev.off()
-  }
 }
--- R/plotSexRatio.fn.R
+++ R/plotSexRatio.fn.R
@@ -5,18 +5,15 @@
 #' @param data.type "length" or "age"
 #' @param main ame that will be used to name the saved png
 #' @param circleSize circle size
-#' @param dopng TRUE/FALSE whether to save a png file
 #' @param ...      Additional arguments for the plots
 #'
 #' @author Allan Hicks and Chantel Wetzel
 #' @importFrom graphics abline
 #' @export
 
-PlotSexRatio.fn <- function(dir, dat, data.type = "length", main = NULL, circleSize = 0.1, dopng = FALSE, ...) {
-  if (dopng) {
-    if (is.null(dir)) {
-      stop("Directory needs to be set.")
-    }
+PlotSexRatio.fn <- function(dir, dat, data.type = "length", main = NULL, circleSize = 0.1,  ...) {
+
+  if(!is.null(dir)) {
     if (!file.exists(dir)) {
       stop("The dir argument leads to a location", ",\ni.e., ", dir, ", that doesn't exist.")
     }
@@ -26,30 +23,62 @@ PlotSexRatio.fn <- function(dir, dat, data.type = "length", main = NULL, circleS
       dir.create(plotdir)
     }
     if (is.null(main)) {
-      png(file.path(dir, paste("/plots/", data.type, "_fraction_female.png", sep = "")), height = 7, width = 7, units = "in", res = 300)
+      png(file.path(dir, "plots", paste(data.type, "_fraction_female.png", sep = "")), height = 7, width = 7, units = "in", res = 300)
     }
     if (!is.null(main)) {
-      png(file.path(dir, paste("/plots/", main, "_", data.type, "_fraction_female.png", sep = "")), height = 7, width = 7, units = "in", res = 300)
+      png(file.path(dir, "plots", paste(main, "_", data.type, "_fraction_female.png", sep = "")), height = 7, width = 7, units = "in", res = 300)
     }
   }
 
-  if (data.type == "length") {
-    temp <- table(dat$Length_cm, dat$Sex)
+  round_any <- function(x, accuracy, f = round) {
+    f(x / accuracy) * accuracy
+  }
+
+  if (data.type == "length") {    
+    bin_width <- 2
+    dat$bin <- round_any(dat$Length_cm, bin_width, floor)
     axis.name <- "Length (cm)"
   }
   if (data.type == "age") {
-    temp <- table(dat$Age, dat$Sex)
-    axis.name <- "Age"
+    bin_with <- 1
+    dat$bin <- round_any(dat$Age, bin_width, floor)
+    axis.name <- "Age (yr)"
   }
+  temp <- table(dat$bin, dat$Sex)
 
   ratioF <- temp[, "F"] / (temp[, "M"] + temp[, "F"])
   nobs <- temp[, "F"] + temp[, "M"]
-  par(mfrow = c(1, 1))
-  plot(x = names(ratioF), y = ratioF, type = "l", col = "red", xlab = axis.name, main = main, ylab = "Fraction female", ...)
-  abline(h = 0.50, col = "grey", lty = 2, lwd = 2)
-  symbols(x = names(ratioF), y = ratioF, circles = nobs, inches = circleSize, fg = "red", bg = rgb(1, 0, 0, alpha = 0.5), add = T)
 
-  if (dopng) {
+  par(mfrow = c(1, 1))
+  plot(x = names(ratioF), y = ratioF, type = "l", col = "red", lty = 2,
+    xlab = axis.name, ylim = c(0, 1), main = main, ylab = "Fraction female", ...)
+  abline(h = 0.50, col = "grey", lty = 2, lwd = 2)
+  symbols(x = names(ratioF), y = ratioF, circles = nobs, 
+    inches = circleSize, fg = "red", bg = rgb(1, 0, 0, alpha = 0.5), 
+    add = TRUE)
+
+  if (!is.null(dir)) {
     dev.off()
+  }
+
+  test <- dplyr::count(dat, bin, Sex) %>%
+    mutate(Proportion = n / sum(n))
+
+  p <- ggplot(test, aes(x = bin, y = Proportion, fill = Sex)) + 
+    geom_bar(position = "fill", stat = "identity") +
+    geom_hline(yintercept = 0.50, col = 'white', lwd = 2) +
+    scale_fill_manual(values = c('F' = 'red', 'M' = 'blue', 'U' = "forestgreen")) +
+    labs(y = "Proportion by Sex", x = axis.name) +
+    theme(panel.border = element_rect(colour = "black", fill = NA, size = 1),
+      panel.background = element_blank(),
+      axis.title.x = element_text (size = 15),
+      axis.title.y = element_text (size = 15),
+      axis.text.x = element_text(size = 15), 
+      axis.text.y = element_text(size = 15))
+  print(p)
+
+  if (!is.null(dir)){
+      ggsave(filename = file.path(dir, "plots", paste0("proportion_by_", data.type, "_sex.png")), 
+        width = 7, height = 7, units = 'in')     
   }
 }
