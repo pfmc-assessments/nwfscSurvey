42f64cd6d757af16362dc166bd364b38f9416fb4
Ian Taylor <ian.taylor@noaa.gov>
2023-03-20 11:24:54 -0700

    fix text after list included in last bullet
    - also change en-dash to hyphen because it was getting formatted wrong

--- R/PullCatch.fn.R
+++ R/PullCatch.fn.R
@@ -45,6 +45,7 @@
 #'   * NWFSC.Santa.Barb.Basin,
 #'   * NWFSC.Shelf.Rockfish (NWFSC.Hook.Line but both are not working),
 #'   * NWFSC.Video.
+#' 
 #' Currently, you must pull data one survey at a time, though we are working on
 #' allowing for a vector of survey names and
 #' `NWFSC.Shelf.Rockfish` and `NWFSC.Hook.Line` are not supported.
