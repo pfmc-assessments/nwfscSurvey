38fe71831d72ee26abcd5af4445a42b13c4e9133
Chantel Wetzel <chantelwetzel@noaa.gov>
2024-03-15 12:37:33 -0700

    new pull_spp function to be consistent with other pull fxns
    This function performs the same as the PullSpp.fn. I thought some additional testing was warrented prior to deprecated the original function.

---
+++ R/pull_spp.R
@@ -0,0 +1,22 @@
+#' Pull species names from the warehouse
+#'
+#' Pull common name and scientific name information from the
+#' data warehouse.
+#' The website is https://www.webapps.nwfsc.noaa.gov/data
+#'
+#' @author Kelli Faye Johnson
+#' @export
+#'
+#' @examples
+#' \dontrun{
+#' spp <- pull_spp()
+#' }
+#'
+pull_spp <- function() {
+  # Get the data from saved .rda file
+  PullSpp <- NULL
+  newenv <- new.env(hash = TRUE, parent = parent.frame())
+  utils::data(species, package = "nwfscSurvey", envir = newenv)
+  species <- get("species", envir = newenv)
+  return(species)
+}
