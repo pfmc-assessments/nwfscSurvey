d2569ae4e4692fbd7e346e9e5ccc8c920967c20b
Chantel Wetzel <chantel.wetzel@noaa.gov>
2024-08-19 13:41:04 -0700

    fix tests to use the new input_n column name

--- tests/testthat/test-comps.R
+++ tests/testthat/test-comps.R
@@ -17,30 +17,33 @@ test_that("get_raw_comps", {
     data = dat,
     comp_bins = seq(16, 80, 4),
     comp_column_name = "Length_cm",
-    two_sex_comps = TRUE
+    two_sex_comps = TRUE,
+    input_sample_size_method = c("stewart_hamel", "tows", "total_samples")[3]
   )
   expect_equal(nrow(length_comps$sexed), 16)
-  expect_equal(sum(length_comps$sexed$nsamp), sum(length_comps$sexed[, 7:ncol(length_comps$sexed)]))
+  expect_equal(sum(length_comps$sexed$input_n), sum(length_comps$sexed[, 7:ncol(length_comps$sexed)]))
   expect_equal(nrow(length_comps$unsexed), 16)
-  expect_equal(sum(length_comps$unsexed$nsamp), sum(length_comps$unsexed[, 7:ncol(length_comps$unsexed)]))
+  expect_equal(sum(length_comps$unsexed$input_n), sum(length_comps$unsexed[, 7:ncol(length_comps$unsexed)]))
 
   length_unsexed_comps <- get_raw_comps(
     data = dat,
     comp_bins = seq(16, 80, 4),
     comp_column_name = "Length_cm",
-    two_sex_comps = FALSE
+    two_sex_comps = FALSE,
+    input_sample_size_method = c("stewart_hamel", "tows", "total_samples")[3]
   )
   expect_equal(nrow(length_unsexed_comps$unsexed), 16)
-  expect_equal(sum(length_unsexed_comps$unsexed$nsamp), sum(length_unsexed_comps$unsexed[, 7:ncol(length_unsexed_comps$unsexed)]))
+  expect_equal(sum(length_unsexed_comps$unsexed$input_n), sum(length_unsexed_comps$unsexed[, 7:ncol(length_unsexed_comps$unsexed)]))
 
   age_comps <- get_raw_comps(
     data = dat,
     comp_bins = 1:12,
     comp_column_name = "Age",
-    two_sex_comps = TRUE
+    two_sex_comps = TRUE,
+    input_sample_size_method = c("stewart_hamel", "tows", "total_samples")[3]
   )
   expect_equal(nrow(age_comps$sexed), 16)
-  expect_equal(sum(age_comps$sexed$nsamp), sum(age_comps$sexed[, 10:ncol(age_comps$sexed)]))
+  expect_equal(sum(age_comps$sexed$input_n), sum(age_comps$sexed[, 10:ncol(age_comps$sexed)]))
   expect_equal(nrow(age_comps$unsexed), 16)
-  expect_equal(sum(age_comps$unsexed$nsamp), sum(age_comps$unsexed[, 10:ncol(age_comps$unsexed)]))
+  expect_equal(sum(age_comps$unsexed$input_n), sum(age_comps$unsexed[, 10:ncol(age_comps$unsexed)]))
 })
