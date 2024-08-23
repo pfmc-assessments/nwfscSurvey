48314362398f08f5a754883a08aaddd4a2c76f7c
Chantel Wetzel <chantel.wetzel@noaa.gov>
2024-08-23 10:49:41 -0700

    add tests

--- tests/testthat/test-comps.R
+++ tests/testthat/test-comps.R
@@ -47,3 +47,55 @@ test_that("get_raw_comps", {
   expect_equal(nrow(age_comps$unsexed), 16)
   expect_equal(sum(age_comps$unsexed$input_n), sum(age_comps$unsexed[, 10:ncol(age_comps$unsexed)]))
 })
+
+test_that("get_expanded_comps", {
+
+  catch <- pull_catch(
+    common_name = "lingcod",
+    years = c(2003, 2018),
+    survey = "NWFSC.Combo",
+    verbose = TRUE
+  )
+  bio <- pull_bio(
+    common_name = "lingcod",
+    years = c(2003, 2018),
+    survey = "NWFSC.Combo",
+    verbose = TRUE
+  )
+  strata <-  CreateStrataDF.fn(
+    names = c("shallow_wa", "shallow_or", "shallow_nca", "shelf_wa", "shelf_or", "shelf_nca"),
+    depths.shallow = c( 55,   55,   55,  183,  183, 183),
+    depths.deep    = c(183,  183,  183,  300,  300, 300),
+    lats.south     = c(46.0, 42.0, 40.0, 46.0, 42.0, 40.0),
+    lats.north     = c(49.0, 46.0, 42.0, 49.0, 46.0, 42.0))
+
+  length_comps <- get_expanded_comps(
+    bio_data = bio,
+    catch_data = catch,
+    comp_bins = seq(14, 80, 4),
+    strata = strata,
+    comp_column_name = "length_cm",
+    two_sex_comps = TRUE,
+    input_sample_size_method = "stewart_hamel",
+    verbose = FALSE
+  )
+  expect_equal(nrow(length_comps$sexed), 16)
+  expect_equal(sum(length_comps$sexed$input_n), 7928)
+  expect_equal(nrow(length_comps$unsexed), 14)
+  expect_equal(sum(length_comps$unsexed$input_n), 587)
+
+  age_comps <- get_expanded_comps(
+    bio_data = bio,
+    catch_data = catch,
+    comp_bins = 0:12,
+    strata = strata,
+    comp_column_name = "age",
+    two_sex_comps = TRUE,
+    input_sample_size_method = "stewart_hamel"
+   )
+  expect_equal(nrow(age_comps$sexed), 15)
+  expect_equal(sum(age_comps$sexed$input_n), 3541)
+  expect_equal(nrow(age_comps$unsexed), 11)
+  expect_equal(sum(age_comps$unsexed$input_n), 45)
+
+})
