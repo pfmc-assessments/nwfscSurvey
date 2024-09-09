f952df88dbd00d006ac9477569be8b4c4a080523
Chantel Wetzel <chantel.wetzel@noaa.gov>
2024-09-09 09:58:08 -0700

    add filtering function

--- R/pull_bio.R
+++ R/pull_bio.R
@@ -11,11 +11,16 @@
 #' @template dir
 #' @template convert
 #' @template verbose
+#' @param standard_filtering A logical TRUE/FALSE that specifies whether data
+#'   should be filtered using the standard filtering which removes tows with bad
+#'   performance (water haul or poor net performance), or stations that have been
+#'   removed from the survey sampling protocol.
 #'
 #' @author Chantel Wetzel
 #' @export
 #'
 #' @import chron
+#' @import cli
 #' @importFrom dplyr rename
 #' @importFrom stringr str_replace_all
 #'
@@ -46,25 +51,28 @@
 #' }
 #'
 pull_bio <- function(
+    survey,
     common_name = NULL,
     sci_name = NULL,
     years = c(1970, 2050),
-    survey,
     dir = NULL,
     convert = TRUE,
-    verbose = TRUE) {
+    verbose = TRUE,
+    standard_filtering = TRUE) {
   options(timeout = 4000000)
   if (survey %in% c("NWFSC.Shelf.Rockfish", "NWFSC.Hook.Line")) {
-    stop(
+    cli::cli_abort(
       "The catch pull currently does not work for NWFSC Hook & Line Survey data.",
-      "\nA subset of the data is available on the data warehouse https://www.webapp.nwfsc.noaa.gov/data",
-      "\nContact John Harms (john.harms@noaa.gov) for the full data set."
+      "A subset of the data is available on the data warehouse https://www.webapp.nwfsc.noaa.gov/data",
+      "Contact John Harms (john.harms@noaa.gov) for the full data set."
     )
   }
 
   if (length(c(common_name, sci_name)) != max(c(length(common_name), length(sci_name)))) {
-    stop("Can not pull data using both the common_name or sci_name together.
-         \n Please retry using only one.")
+    cli::cli_abort(
+      "Function is unable to pull data using both the common_name or sci_name together.
+      Please retry using only one."
+    )
   }
 
   check_dir(dir = dir, verbose = verbose)
@@ -87,14 +95,11 @@ pull_bio <- function(
     years <- c(years, years)
   }
 
-  vars_short <- c(
+  vars_long <- c(
     "project", "trawl_id", "common_name", "scientific_name", "year", "vessel", "pass",
     "tow", "datetime_utc_iso", "depth_m", "weight_kg", "ageing_lab", "otosag_id",
-    "length_cm", "width_cm", "sex", "age_years", "latitude_dd", "longitude_dd"
-  )
-
-  vars_long <- c(
-    vars_short,
+    "length_cm", "width_cm", "sex", "age_years", "latitude_dd", "longitude_dd",
+    "performance", "station_invalid",
     "ageing_laboratory_dim$laboratory",
     "standard_survey_age_indicator",
     "standard_survey_length_or_width_indicator",
@@ -102,7 +107,6 @@ pull_bio <- function(
     "operation_dim$legacy_performance_code"
   )
 
-  # symbols here are generally: %22 = ", %2C = ",", %20 = " "
   species_str <- convert_to_hex_string(species)
   add_species <- paste0("field_identified_taxonomy_dim$", var_name, "|=[", species_str, "]")
 
@@ -119,7 +123,9 @@ pull_bio <- function(
   )
 
   if (verbose) {
-    message("Pulling biological data. This can take up to ~ 30 seconds (or more).")
+    cli::cli_alert_info(
+      "Pulling biological data for {species}. This can take up to ~ 30 seconds (or more)."
+    )
   }
   bio_pull <- try(get_json(url = url_text))
 
@@ -137,42 +143,60 @@ pull_bio <- function(
   # the AFSC.Slope and Triennial survey since lengths are checked later in the
   # length_fact data table.
   if (!is.null(dim(bio_pull))) {
+    if (survey %in% c("Triennial", "AFSC.Slope")) {
+      data_text <- "age/otolith samples"
+    } else {
+      data_text <-"biological samples"
+    }
+    bio_pull <- filter_pull(
+      data = bio_pull,
+      data_type = data_text,
+      standard_filtering = standard_filtering,
+      verbose = verbose)
+
     # Filter out non-standard samples
     # Some early entries are NA for standard sample indicators. These should be retained.
     standard_lengths <- bio_pull[, "standard_survey_length_or_width_indicator"] %in% c(NA, "NA", "Standard Survey Length or Width")
     if (length(standard_lengths) != dim(bio_pull)[1]) {
-      bio_pull <- bio_pull[standard_lengths, ]
+      if (verbose) {
+        n <- dim(bio_pull)[1] - length(standard_lengths)
+        cli::cli_alert_info(
+          "There were {n} lengths that were collected outside standard sampling protocol."
+        )
+      }
+      if (standard_filtering) {
+        bio_pull <- bio_pull[standard_lengths, ]
+      }
     }
 
     # Remove non-standard ages
     nonstandard_age <- which(bio_pull[, "standard_survey_age_indicator"] == "Not Standard Survey Age")
     if (length(nonstandard_age) > 0) {
-      bio_pull[nonstandard_age, "age_years"] <- NA
+      if (verbose) {
+        cli::cli_alert_info(
+          "There were {length(nonstandard_age)} ages that were collected outside standard sampling protocol."
+        )
+      }
+      if (standard_filtering) {
+        bio_pull[nonstandard_age, "age_years"] <- NA
+      }
     }
 
     # Remove non-standard weights
     nonstandard_wgt <- which(bio_pull[, "standard_survey_weight_indicator"] == "Not Standard Survey Weight")
     if (length(nonstandard_wgt) > 0) {
-      bio_pull[nonstandard_wgt, "weight_kg"] <- NA
+      if (verbose) {
+        cli::cli_alert_info(
+          "There were {length(nonstandard_wgt)} weights that were collected outside standard sampling protocol."
+        )
+      }
+      if (standard_filtering) {
+        bio_pull[nonstandard_wgt, "weight_kg"] <- NA
+      }
     }
 
-    fill_in  <- is.na(bio_pull[, "operation_dim$legacy_performance_code"])
-    if (sum(fill_in) > 0) {
-      bio_pull[fill_in, "operation_dim$legacy_performance_code"] <- -999
-    }
-    # A value of 8 in the Triennial data indicates a water haul
-    good_tows <- bio_pull[, "operation_dim$legacy_performance_code"] != 8
-    if (sum(good_tows) != dim(bio_pull)[1]) {
-      bio_pull <- bio_pull[good_tows, ]
-    }
-    find <- colnames(bio_pull) == "ageing_laboratory_dim$laboratory"
-    colnames(bio_pull)[find] <- "ageing_lab"
-
-    # Remove the extra columns now that they are not needed
-    bio_pull <- bio_pull[, vars_short]
-    # These two columns are added for functionality since the older data
-    # pulls were weight and age and the subsequent functions have not been
-    # revised.
+    colnames(bio_pull)[colnames(bio_pull) == "standard_survey_length_or_width_indicator"] <- "ageing_lab"
+    colnames(bio_pull)[colnames(bio_pull) == "operation_dim$legacy_performance_code"] <- "legacy_performance_code"
     bio_pull$weight <- bio_pull$weight_kg
     bio_pull$age <- bio_pull$age_years
     bio_pull$date <- chron::chron(format(as.POSIXlt(bio_pull$datetime_utc_iso, format = "%Y-%m-%dT%H:%M:%S"), "%Y-%m-%d"), format = "y-m-d", out.format = "YYYY-m-d")
@@ -202,20 +226,18 @@ pull_bio <- function(
     }
 
     if (is.data.frame(len_pull)) {
-      fill_in <- is.na(len_pull[, "operation_dim$legacy_performance_code"])
-      if (sum(fill_in) > 0) {
-        len_pull[fill_in, "operation_dim$legacy_performance_code"] <- -999
-      }
-      # Remove water hauls
-      good_tows <- len_pull[, "operation_dim$legacy_performance_code"] != 8
-      if (sum(good_tows) != dim(len_pull)[1]) {
-        len_pull <- len_pull[good_tows, ]
-      }
+      len_pull <- filter_pull(
+        data = len_pull,
+        data_type = "length samples",
+        standard_filtering = standard_filtering,
+        verbose = verbose)
 
       len_pull$weight_kg <- len_pull$weight <- NA
       len_pull$date <- chron::chron(format(as.POSIXlt(len_pull$datetime_utc_iso, format = "%Y-%m-%dT%H:%M:%S"), "%Y-%m-%d"), format = "y-m-d", out.format = "YYYY-m-d")
       len_pull$trawl_id <- as.character(len_pull$trawl_id)
-      len_pull <- len_pull[, colnames(len_pull) != "operation_dim$legacy_performance_code"]
+      #len_pull <- len_pull[, colnames(len_pull) != "operation_dim$legacy_performance_code"]
+      colnames(len_pull)[colnames(len_pull) == "standard_survey_length_or_width_indicator"] <- "ageing_lab"
+      colnames(len_pull)[colnames(len_pull) == "operation_dim$legacy_performance_code"] <- "legacy_performance_code"
     }
 
     bio <- list()
