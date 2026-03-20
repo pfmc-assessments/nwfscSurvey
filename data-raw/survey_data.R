740b13dc9afa93ae0829b911a4222a7b3c51b0c1
Chantel Wetzel - NOAA Federal <chantel.wetzel@noaa.gov>
2026-03-20 14:45:52 -0700

    update data objects

---
+++ data-raw/survey_data.R
@@ -0,0 +1,8 @@
+catch_nwfsc_combo <- pull_catch(common_name = "lingcod", years = 2003:2019)
+bio_nwfsc_combo <- pull_catch(common_name = "lingcod", years = 2003:2019)
+
+usethis::use_data(
+  catch_nwfsc_combo,
+  bio_nwfsc_combo,
+  overwrite = TRUE
+)
