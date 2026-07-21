3122cf818cf7d0737bfb889f1404fac3ebd13622
iantaylor-NOAA <ian.taylor@noaa.gov>
2026-07-21 10:43:48 -0700

    clean up roxygen comments for deprecated functions

--- R/Format.AKSlope.fn.R
+++ R/Format.AKSlope.fn.R
@@ -1,12 +1,7 @@
-#' Rename AFSC slope survey columns from data pulled before 2017
+#' Deprecated function replaced by pull_catch() and pull_bio()
 #'
 #' @details
-#' Rename columns in the AFSC slope survey data file
-#' received prior to the creation of the NWFSC data
-#' warehouse. This function converts the older data files
-#' to create the needed column names to work within survey
-#' package functions. Output from this function will be list
-#' of containing catch, length, and age data.
+#' This function is no longer used.  Please use pull_catch() and pull_bio() to get properly formatted and filtered data.
 #'
 #' @inheritParams pull_catch
 #' @param datTows A data frame of catch data for the
@@ -21,22 +16,6 @@
 #'
 #' @author Chantel Wetzel
 #' @export
-#' @family helper function
-#'
-#' @examples
-#' \dontrun{
-#' # load data files for catch and biological data
-#' load("Tri.Shelf.and.AFSC.Slope.canary.Catch.24.May.11.dmp")
-#' catch <- Tri.Shelf.and.AFSC.Slope.canary.Catch.24.May.11
-#' load("AFSC.Slope.Shelf.sable.bio.5.24.11.dmp")
-#' bio <- AK.Surveys.Bio.sablefish.24.May.11
-#' # call function and reformat the data
-#' filter.dat <- Format.AKSlope.fn(
-#'   datTows = catch,
-#'   datL = bio,
-#'   start.year = 1997
-#' )
-#' }
 #'
 Format.AKSlope.fn <- function(
   dir = NULL,
--- R/ReadInLengths.R
+++ R/ReadInLengths.R
@@ -1,12 +1,5 @@
-#' Reads in the compositional survey data and filters the data into what is necessary
-#' It reads in catch and station data and makes sure only the species necessary are kept
-#' ### may want to keep NA (blank in Excel) to select the zero tows
-#' removeCAN is a flag if you want tows in Canadian waters removed
-#'    need the file called foreign_hauls.csv
+#' Deprecated function replaced by pull_bio()
 #'
-#' Necessary column names
-#'    SPECIES_CODE
-#'    LENGTH
 #' @inheritParams pull_catch
 #' @param dat data file name
 #'
