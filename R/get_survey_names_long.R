d8bfe52b2cca5679e5983b46d1fcbbdb759acf72
Chantel Wetzel - NOAA Federal <chantel.wetzel@noaa.gov>
2026-08-14 13:54:34 -0700

    update project names

--- R/get_survey_names_long.R
+++ R/get_survey_names_long.R
@@ -10,43 +10,47 @@ get_survey_names_long <- function() {
   # AFSC.slope - not yet in warehouse - Conducted between 1988 - 2001, full sampling in the 1997, 1999, 2000, and 2001 years
   # NWFSC.Combo - Groundfish Slope and Shelf Combination Survey - Conducted starting in 2003 - present
   # NWFSC.Shelf - Groundfish Shelf Survey - Only conducted in 2001 (not used in West Coast groundfish assessments)
-  matrix(
+  a <- matrix(
     data = c(
       "Triennial",
-      "Groundfish Triennial Shelf Survey",
-      "Triennial Survey",
+      "AFSC/RACE Triennial Groundfish Shelf Survey",
+
+      "Triennial",
+      "AFSC/RACE Triennial Groundfish Shelf Survey (by NWFSC)",
+
       "AFSC.Slope",
       "AFSC/RACE Slope Survey",
-      "AFSC Slope Survey",
-      "NWFSC.Combo",
-      "Groundfish Slope and Shelf Combination Survey",
-      "NWFSC WCGBTS",
+
       "NWFSC.Slope",
-      "Groundfish Slope Survey",
-      "NWFSC Slope Survey",
+      "West Coast Groundfish Bottom Trawl Slope Survey",
+
+      "NWFSC.Combo",
+      "West Coast Groundfish Bottom Trawl Slope/Shelf Combination Survey",
+
       "NWFSC.Shelf",
-      "Groundfish Shelf Survey",
-      "NWFSC Shelf Survey",
+      "West Coast Groundfish Bottom Trawl Shelf Survey",
+
       "NWFSC.Hypoxia",
-      "Hypoxia Study",
-      "",
+      "West Coast Groundfish Bottom Trawl Hypoxia Study",
+
       "NWFSC.Santa.Barb.Basin",
-      "Santa Barbara Basin Study",
-      "",
+      "West Coast Groundfish Bottom Trawl Santa Barbara Basin Study",
+
       "NWFSC.Shelf.Rockfish",
       "Shelf Rockfish [2004-2015]",
-      "",
+
       "NWFSC.Hook.Line",
       "Hook and Line",
-      "NWFSC Hook and Line Survey",
+
       "NWFSC.Video",
-      "Video Study",
-      "",
+      "West Coast Groundfish Bottom Trawl Video Study",
+
       "Triennial.Canada",
-      "Triennial Shelf Groundfish Survey: Canada",
-      ""
+      "AFSC/RACE Triennial Groundfish Shelf Survey: Canada"
     ),
-    ncol = 3,
+    ncol = 2,
     byrow = TRUE
   )
+  colnames(a) <- c("old_names", "new_names")
+  return(a)
 }
